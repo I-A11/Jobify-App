664056085d461c3fbea3a86a1315120a21e21df7
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-08-06 19:26:32 +1000

    Create post request and use morgan package.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import express from "express";
 const app = express();
 
+import morgan from "morgan";
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
 });
 
 app.listen(5100, () => {
